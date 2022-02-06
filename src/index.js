b930657de68a0e67b867c95ed3e46b2162fa38f7
Anton Danilov <anton2009danilov@yandex.ru>
2022-02-06 05:52:32 +0300

    modify bootstrap imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './css/style.css';
 import app from './app.js';
 
