307c8f78e62e960d22c0079722df23880ff8eac9
Anton Danilov <anton2009danilov@yandex.ru>
2022-10-07 22:29:30 +0300

    refactor: resolve duplication in parsers module

--- src/error-handlers.js
+++ src/error-handlers.js
@@ -2,36 +2,34 @@ import onChange from 'on-change';
 import i18next from 'i18next';
 import render from './view.js';
 
-const handleNetworkError = (state, e) => {
+const changeUiState = (state, message) => {
   const watchedState = onChange(state, () => {});
   watchedState.ui.input.isValid = false;
-  watchedState.ui.message = i18next.t('yup.errors.networkError');
+  watchedState.ui.message = message;
+};
+
+const handleNetworkError = (state, e) => {
+  changeUiState(state, i18next.t('yup.errors.networkError'));
   render(state);
   throw (e);
 };
 
 const hanldeInvalidRssError = (state, e) => {
   if (e.message !== 'Network Error') {
-    const watchedState = onChange(state, () => {});
-    watchedState.ui.input.isValid = false;
-    watchedState.ui.message = i18next.t('yup.errors.invalidRss');
+    changeUiState(state, i18next.t('yup.errors.invalidRss'));
     render(state);
     throw (e);
   }
 };
 
 const handleInvalidUrlError = (state) => {
-  const watchedState = onChange(state, () => {});
-  watchedState.ui.message = i18next.t('yup.errors.invalidUrl');
-  watchedState.ui.input.isValid = false;
+  changeUiState(state, i18next.t('yup.errors.invalidUrl'));
   render(state);
 };
 
 const checkForEmptyRssUrlError = (state, url) => {
   if (!url) {
-    const watchedState = onChange(state, () => {});
-    watchedState.ui.input.isValid = false;
-    watchedState.ui.message = i18next.t('yup.errors.emptyRssUrl');
+    changeUiState(state, i18next.t('yup.errors.emptyRssUrl'));
     return true;
   }
 
@@ -43,9 +41,7 @@ const checkForAlreadyExistsError = (state, url) => {
   const isRepeated = feeds.some((feed) => feed.url === url);
 
   if (isRepeated) {
-    const watchedState = onChange(state, () => {});
-    watchedState.ui.message = i18next.t('yup.errors.alreadyExists');
-    watchedState.ui.input.isValid = false;
+    changeUiState(state, i18next.t('yup.errors.alreadyExists'));
     render(state);
     return true;
   }
