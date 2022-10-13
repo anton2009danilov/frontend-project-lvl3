12e075778b2a08927ebc2ff871db38349d6fe531
Anton Danilov <anton2009danilov@yandex.ru>
2022-10-13 09:20:01 +0300

    refactor: put all app.js module functions inside app function

--- src/app.js
+++ src/app.js
@@ -6,184 +6,184 @@ import ru from './locales/ru.js';
 import render from './view.js';
 import { parseRssFromHtml, parseUpdatedRssHtml } from './parsers.js';
 
-const state = {
-  form: {
-    isRefreshed: true,
-    message: null,
-    input: {
-      isValid: true,
+const app = () => {
+  const state = {
+    form: {
+      isRefreshed: true,
+      message: null,
+      input: {
+        isValid: true,
+      },
     },
-  },
-  ui: {
-    isStateWatched: false,
-  },
-  rss: {
-    feeds: [],
-    posts: [],
-  },
-};
+    ui: {
+      isStateWatched: false,
+    },
+    rss: {
+      feeds: [],
+      posts: [],
+    },
+  };
 
-const view = render(state);
+  const view = render(state);
 
-i18next.init({
-  lng: 'ru',
-  debug: true,
-  resources: {
-    ru,
-  },
-});
-
-const changeUiState = (message) => {
-  view.form.input.isValid = false;
-  view.form.message = message;
-};
-
-const handleNetworkError = (e) => {
-  changeUiState(i18next.t('yup.errors.networkError'));
-  throw (e);
-};
-
-const handleInvalidRssError = (e) => {
-  if (e.message !== 'Network Error') {
-    changeUiState(i18next.t('yup.errors.invalidRss'));
-    throw (e);
-  }
-};
-
-const handleInvalidUrlError = () => {
-  changeUiState(i18next.t('yup.errors.invalidUrl'));
-};
-
-const checkForEmptyRssUrlError = (url) => {
-  if (!url) {
-    changeUiState(i18next.t('yup.errors.emptyRssUrl'));
-    return true;
-  }
-
-  return false;
-};
-
-const checkForAlreadyExistsError = (url) => {
-  const { feeds } = view.rss;
-  const isRepeated = feeds.some((feed) => feed.url === url);
-
-  if (isRepeated) {
-    changeUiState(i18next.t('yup.errors.alreadyExists'));
-    return true;
-  }
-
-  return false;
-};
-
-const validateUrl = (url) => {
-  const urlSchema = object({
-    url: string().url(),
+  i18next.init({
+    lng: 'ru',
+    debug: true,
+    resources: {
+      ru,
+    },
   });
 
-  return urlSchema.validate(url);
-};
+  const changeUiState = (message) => {
+    view.form.input.isValid = false;
+    view.form.message = message;
+  };
 
-const getRssHtml = (url) => {
-  const parser = new DOMParser();
+  const handleNetworkError = (e) => {
+    changeUiState(i18next.t('yup.errors.networkError'));
+    throw (e);
+  };
 
-  return axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-    .then((response) => response.data)
-    .then((data) => parser.parseFromString(data.contents, 'text/xml'));
-};
+  const handleInvalidRssError = (e) => {
+    if (e.message !== 'Network Error') {
+      changeUiState(i18next.t('yup.errors.invalidRss'));
+      throw (e);
+    }
+  };
 
-const addNewRss = (url) => {
-  const { form, rss } = view;
+  const handleInvalidUrlError = () => {
+    changeUiState(i18next.t('yup.errors.invalidUrl'));
+  };
 
-  if (checkForEmptyRssUrlError(url)) {
-    return;
-  }
+  const checkForEmptyRssUrlError = (url) => {
+    if (!url) {
+      changeUiState(i18next.t('yup.errors.emptyRssUrl'));
+      return true;
+    }
 
-  getRssHtml(url)
-    .catch((e) => handleNetworkError(e))
-    .then((rssHtml) => {
-      const { feeds, posts } = rss;
+    return false;
+  };
 
-      const newFeedId = _.isEmpty(feeds) ? 1 : _.last(feeds).id + 1;
-      const newPostId = _.isEmpty(posts) ? 1 : _.last(posts).id + 1;
+  const checkForAlreadyExistsError = (url) => {
+    const { feeds } = view.rss;
+    const isRepeated = feeds.some((feed) => feed.url === url);
 
-      const newRss = parseRssFromHtml(rssHtml, url);
+    if (isRepeated) {
+      changeUiState(i18next.t('yup.errors.alreadyExists'));
+      return true;
+    }
 
-      newRss.feeds = newRss.feeds.map((feed) => _.set(feed, 'id', newFeedId));
-      const newPostsUnsorted = newRss.posts.map((post, index) => ({
-        ...post,
-        feedId: newFeedId,
-        id: newPostId + index,
-      }));
+    return false;
+  };
 
-      newRss.posts = _.sortBy(newPostsUnsorted, (post) => (post.pubDate));
+  const validateUrl = (url) => {
+    const urlSchema = object({
+      url: string().url(),
+    });
 
-      _.set(
-        view,
-        'rss',
-        {
-          feeds: _.concat(rss.feeds, newRss.feeds),
-          posts: _.concat(rss.posts, newRss.posts),
-        },
-      );
+    return urlSchema.validate(url);
+  };
 
-      form.input.isValid = true;
-      form.message = i18next.t('yup.success');
-    })
-    .catch((e) => handleInvalidRssError(e));
-};
+  const getRssHtml = (url) => {
+    const parser = new DOMParser();
 
-const updateRss = () => {
-  view.rss.feeds.forEach((feed, index) => {
-    const { url } = feed;
+    return axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+      .then((response) => response.data)
+      .then((data) => parser.parseFromString(data.contents, 'text/xml'));
+  };
+
+  const addNewRss = (url) => {
+    const { form, rss } = view;
+
+    if (checkForEmptyRssUrlError(url)) {
+      return;
+    }
 
     getRssHtml(url)
-      .then((rssHtml) => parseUpdatedRssHtml(rssHtml, feed, index))
-      .then(([changedFeed, updatedFeedIndex, posts]) => {
-        const { id } = changedFeed;
-        const currentPosts = _.filter(view.rss.posts, ({ feedId }) => feedId === id);
-        const diffPosts = _.differenceWith(posts, currentPosts.map((el) => _.omit(el, ['id', 'isRead'])), _.isEqual);
+      .catch((e) => handleNetworkError(e))
+      .then((rssHtml) => {
+        const { feeds, posts } = rss;
 
-        if (!_.isEmpty(diffPosts)) {
-          const lastPostId = _.isEmpty(view.rss.posts)
-            ? 0
-            : _.last(view.rss.posts).id;
+        const newFeedId = _.isEmpty(feeds) ? 1 : _.last(feeds).id + 1;
+        const newPostId = _.isEmpty(posts) ? 1 : _.last(posts).id + 1;
 
-          const newPosts = _.sortBy(diffPosts, (post) => (post.pubDate))
-            .map((post, postIndex) => {
-              const newPostId = lastPostId + postIndex + 1;
-              return _.set(post, 'id', newPostId);
-            });
+        const newRss = parseRssFromHtml(rssHtml, url);
 
-          const updatedFeeds = _.set(
-            _.clone(view).rss.feeds,
-            updatedFeedIndex,
-            changedFeed,
-          );
+        newRss.feeds = newRss.feeds.map((feed) => _.set(feed, 'id', newFeedId));
+        const newPostsUnsorted = newRss.posts.map((post, index) => ({
+          ...post,
+          feedId: newFeedId,
+          id: newPostId + index,
+        }));
 
-          const updatedPosts = _.concat(view.rss.posts, newPosts);
+        newRss.posts = _.sortBy(newPostsUnsorted, (post) => (post.pubDate));
 
-          _.set(
-            view,
-            'rss',
-            {
-              feeds: updatedFeeds,
-              posts: updatedPosts,
-            },
-          );
-        }
+        _.set(
+          view,
+          'rss',
+          {
+            feeds: _.concat(rss.feeds, newRss.feeds),
+            posts: _.concat(rss.posts, newRss.posts),
+          },
+        );
+
+        form.input.isValid = true;
+        form.message = i18next.t('yup.success');
       })
-      .catch((e) => { throw (e); });
-  });
-};
+      .catch((e) => handleInvalidRssError(e));
+  };
 
-const watchForUpdates = () => {
-  const timeStep = 5000;
-  setTimeout(watchForUpdates, timeStep);
+  const updateRss = () => {
+    view.rss.feeds.forEach((feed, index) => {
+      const { url } = feed;
 
-  updateRss();
-};
+      getRssHtml(url)
+        .then((rssHtml) => parseUpdatedRssHtml(rssHtml, feed, index))
+        .then(([changedFeed, updatedFeedIndex, posts]) => {
+          const { id } = changedFeed;
+          const currentPosts = _.filter(view.rss.posts, ({ feedId }) => feedId === id);
+          const diffPosts = _.differenceWith(posts, currentPosts.map((el) => _.omit(el, ['id', 'isRead'])), _.isEqual);
 
-const app = () => {
+          if (!_.isEmpty(diffPosts)) {
+            const lastPostId = _.isEmpty(view.rss.posts)
+              ? 0
+              : _.last(view.rss.posts).id;
+
+            const newPosts = _.sortBy(diffPosts, (post) => (post.pubDate))
+              .map((post, postIndex) => {
+                const newPostId = lastPostId + postIndex + 1;
+                return _.set(post, 'id', newPostId);
+              });
+
+            const updatedFeeds = _.set(
+              _.clone(view).rss.feeds,
+              updatedFeedIndex,
+              changedFeed,
+            );
+
+            const updatedPosts = _.concat(view.rss.posts, newPosts);
+
+            _.set(
+              view,
+              'rss',
+              {
+                feeds: updatedFeeds,
+                posts: updatedPosts,
+              },
+            );
+          }
+        })
+        .catch((e) => { throw (e); });
+    });
+  };
+
+  const watchForUpdates = () => {
+    const timeStep = 5000;
+    setTimeout(watchForUpdates, timeStep);
+
+    updateRss();
+  };
+
   const form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
