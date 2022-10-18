452f05c56dc3f8d61987f464ce1cceb1a7e74006
Anton Danilov <anton2009danilov@yandex.ru>
2022-10-18 14:17:06 +0300

    fix: delete unnecessary ".then"

--- src/app.js
+++ src/app.js
@@ -29,8 +29,7 @@ const getRssHtml = (url) => {
   const parser = new DOMParser();
 
   return axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-    .then((response) => response.data)
-    .then((data) => parser.parseFromString(data.contents, 'text/xml'));
+    .then((response) => parser.parseFromString(response.data.contents, 'text/xml'));
 };
 
 const app = () => {
